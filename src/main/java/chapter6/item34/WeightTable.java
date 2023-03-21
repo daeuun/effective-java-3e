421acf61699bf3c81d20f392441d91e01b35a831
daeuun <leeda51925@gmail.com>
2023-03-22 00:11:49 +0900

    enum type with data and methods

--- src/main/java/chapter6/item34/WeightTable.java
+++ src/main/java/chapter6/item34/WeightTable.java
@@ -1,2 +1,11 @@
-package chapter6.item34;public class WeightTable {
+package chapter6.item34;
+
+public class WeightTable {
+    public static void main(String[] args) {
+        double earthWeight = Double.parseDouble(args[0]);
+        double mass = earthWeight / Planet.EARTH.surfaceGravity();
+        for (Planet planet : Planet.values()) {
+            System.out.printf("%s에서의 무게는 %f이다. %n", planet, planet.surfaceWeight(mass));
+        }
+    }
 }
